9a8e2babf6ecdeb252a9eb8a75ccb5b73cd8c0e4
James Twisleton <james@consultwiz.biz>
2024-01-25 12:40:49 +0000

    bringing mongodb into terraform

---
+++ app/terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "mongodbatlas_public_key" {
+  description = "MongoDB Atlas Public Key"
+  type        = string
+}
+
+variable "mongodbatlas_private_key" {
+  description = "MongoDB Atlas Private Key"
+  type        = string
+}
+
+variable "aws_region" {
+  description = "AWS Region"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "AWS Access Key"
+  type        = string
+}
+
+variable "aws_secret_access_key" {
+  description = "AWS Secret Key"
+  type        = string
+}
