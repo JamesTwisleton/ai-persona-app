60bc93eed7c104282e874b178c34295efb3a82dd
James Twisleton <james@consultwiz.biz>
2024-01-25 00:18:00 +0000

    ecs stack provisioning (not working)

--- app/terraform/main.tf
+++ app/terraform/main.tf
@@ -64,11 +64,34 @@ resource "aws_ecr_repository" "ai_persona_app_ecr_repo" {
   }
 }
 
-output "ai_persona_app_ecr_repo_url" {
-  value = aws_ecr_repository.ai_persona_app_ecr_repo.repository_url
+# TODO: split the below into a different terraform file, must have ECR repo and have pushed image to it before trying to set up ECS
+resource "aws_iam_role" "ecs_execution_role" {
+  name = "ecs_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+      },
+    ],
+  })
 }
 
-# TODO: split the below into a different terraform file, must have ECR repo and have pushed image to it before trying to set up ECS
+resource "aws_iam_role_policy_attachment" "ecs_execution_role_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_read_policy" {
+  role       = aws_iam_role.ecs_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
 resource "aws_ecs_cluster" "ai_persona_app_cluster" {
   name = "ai-persona-app"
 }
@@ -79,16 +102,164 @@ resource "aws_ecs_task_definition" "ai_persona_app_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_execution_role.arn
 
   container_definitions = jsonencode([{
     name  = "ai-persona-app-container",
-    image = "ai_persona_app_ecr_repo_url:latest",
+    # TODO: figure out doing this dynamically, actual deployment should be done with GitHub Actions
+    # so it shouldn't be a problem - need to make Actions remove latest tag and tag new image
+    # with latest, otherwise running this terraform will revert the container image to whatever
+    # latest tag it finds (could be multiple)
+    image = "${aws_ecr_repository.ai_persona_app_ecr_repo.repository_url}:latest",
     portMappings = [{
       containerPort = 3000,
-      hostPort      = 80
+      hostPort      = 3000
     }]
   }])
 }
+
+resource "aws_vpc" "app_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "app-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "app_igw" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  tags = {
+    Name = "app-internet-gateway"
+  }
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.app_vpc.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.app_igw.id
+}
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id            = aws_vpc.app_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id            = aws_vpc.app_vpc.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-2"
+  }
+}
+
+resource "aws_security_group" "alb_security_group" {
+  name        = "alb-security-group"
+  description = "Security group for ALB"
+  vpc_id      = aws_vpc.app_vpc.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_lb" "public_load_balancer" {
+  name               = "public-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_security_group.id]
+  subnets            = [aws_subnet.public_subnet_1.id, aws_subnet.public_subnet_2.id]
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "public-load-balancer"
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name     = "target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.app_vpc.id
+  target_type = "ip"
+
+  health_check {
+    enabled             = true
+    path                = "/api/health"  # Update this if your Next.js app has a specific health check path
+    protocol            = "HTTP"
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
+  }
+}
+
+resource "aws_lb_listener" "front_end_listener" {
+  load_balancer_arn = aws_lb.public_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn
+  }
+}
+
+resource "aws_ecs_service" "ai_persona_app_service" {
+  name            = "ai-persona-app-service"
+  cluster         = aws_ecs_cluster.ai_persona_app_cluster.id
+  task_definition = aws_ecs_task_definition.ai_persona_app_task.arn
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets = [
+      aws_subnet.public_subnet_1.id, 
+      aws_subnet.public_subnet_2.id
+    ]
+    security_groups = [aws_security_group.alb_security_group.id]
+  }
+
+  desired_count = 1
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn
+    container_name   = "ai-persona-app-container"
+    container_port   = 3000
+  }
+
+  depends_on = [
+    aws_lb_listener.front_end_listener
+  ]
+
+  # lifecycle {
+  #   ignore_changes = [
+  #     task_definition  # Ignoring changes to prevent Terraform from replacing the service on every new task definition revision.
+  #   ]
+  # }
+}
+
+
 
 
 
