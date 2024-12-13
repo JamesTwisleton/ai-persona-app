5d352b710dc0b54a83949133459758ee5817b203
James Twisleton <james@consultwiz.biz>
2024-12-13 20:37:22 +0000

    added github workflow and rebuild docker image option

--- code/python/compose_docker.sh
+++ code/python/compose_docker.sh
@@ -2,8 +2,17 @@
 
 IMAGE_NAME="persona-compass-app"
 
-# Check if the image exists
-if docker image inspect "$IMAGE_NAME" > /dev/null 2>&1; then
+# Check if the --rebuild flag is passed
+FORCE_REBUILD=false
+if [[ "$1" == "--rebuild" ]]; then
+  FORCE_REBUILD=true
+fi
+
+# Build or use existing image
+if $FORCE_REBUILD; then
+  echo "Force rebuild option selected. Building the image..."
+  docker build -t "$IMAGE_NAME" .
+elif docker image inspect "$IMAGE_NAME" > /dev/null 2>&1; then
   echo "Image $IMAGE_NAME already exists. Running it..."
 else
   echo "Image $IMAGE_NAME not found. Building it..."
