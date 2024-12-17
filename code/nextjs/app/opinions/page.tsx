9ebbfd7671221e73d9b3f9be4cbb84e9f6d3d4d9
James Twisleton <james@consultwiz.biz>
2024-12-17 21:08:43 +0000

    extract DarkModeToggle into component

--- code/nextjs/app/opinions/page.tsx
+++ code/nextjs/app/opinions/page.tsx
@@ -1,40 +1,9 @@
 "use client";
 
-import { useState, useEffect } from "react";
 import PersonaDescription from "@/components/PersonaDescription";
+import DarkModeToggle from "@/components/DarkModeToggle";
 
 export default function Page() {
-  // TODO: extract this light/dark mode toggle functionality elsewhere
-  const [isLightMode, setIsLightMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    const htmlElement = document.documentElement;
-
-    if (isLightMode) {
-      htmlElement.classList.remove("light-mode");
-      htmlElement.classList.add("dark-mode");
-    } else {
-      htmlElement.classList.remove("dark-mode");
-      htmlElement.classList.add("light-mode");
-    }
-
-    setIsLightMode(!isLightMode);
-  };
-
-  // Set initial mode based on system preference
-  useEffect(() => {
-    const htmlElement = document.documentElement;
-    const prefersDark = window.matchMedia(
-      "(prefers-color-scheme: dark)",
-    ).matches;
-
-    if (prefersDark) {
-      htmlElement.classList.add("dark-mode");
-    } else {
-      htmlElement.classList.add("light-mode");
-    }
-  }, []);
-
   // TODO: get these values from call to flask API
   const katie = {
     index: 1,
@@ -122,12 +91,7 @@ export default function Page() {
   return (
     <div>
       <div className="col-span-1 text-end">
-        <button
-          onClick={toggleDarkMode}
-          className="mt-4 mb-5 mr-5 px-4 py-2 rounded bg-blue-500 text-white hover:bg-blue-700"
-        >
-          Toggle to {isLightMode ? "Dark" : "Light"} Mode
-        </button>
+        <DarkModeToggle />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-3 mb-10">
         {personas.map((persona) => (
