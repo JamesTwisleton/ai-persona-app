0097b479d6956f13ad72c9b7edbab670f85f0744
James Twisleton <james@consultwiz.biz>
2024-12-23 23:15:13 +0000

    persisting on close

--- code/python/flask_app/app/config.py
+++ code/python/flask_app/app/config.py
@@ -1,9 +1,21 @@
+from dotenv import load_dotenv
 import os
 
-# Remember to set in .env file
+# Load the .env file
+load_dotenv()
+
 class Config:
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    DEBUG = os.getenv('FLASK_DEBUG', 'False') == 'True'
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{os.path.join(BASE_DIR, "database.db")}'
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "SQLALCHEMY_DATABASE_URI",
+        f"sqlite:///{os.path.join(BASE_DIR, 'data', 'database.db')}"
+    )
+    # SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+    @staticmethod
+    def validate():
+        # Check required variables
+        if not Config.OPENAI_API_KEY:
+            raise ValueError("OPENAI_API_KEY is not set in the environment or .env file.")
