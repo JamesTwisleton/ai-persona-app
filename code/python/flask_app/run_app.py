4d4ef2e47dd255ed8edafa78e75832c4909c8d81
James Twisleton <james@consultwiz.biz>
2025-01-04 19:09:57 +0000

    debug and port from .env, initial logging

--- code/python/flask_app/run_app.py
+++ code/python/flask_app/run_app.py
@@ -2,7 +2,3 @@ from app import create_app
 
 # Create the application instance
 app = create_app()
-
-# Run the application
-if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=8050)
